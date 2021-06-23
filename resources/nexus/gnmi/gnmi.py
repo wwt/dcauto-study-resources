8eb9f5fe21db94a148f1c9ea8eb863f910c58c0a
Timothy Hull <timothyhull@gmail.com>
2021-06-23 07:12:25 +0000

    Organized code and added additional comments

--- resources/nexus/gnmi/gnmi.py
+++ resources/nexus/gnmi/gnmi.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+""" Simple script to consume model-driven telemetry from an open NX-OS
+    switch using gNMI.
+
+    Requires:
+        - .pem certificate exported from the NX-OS gNMI configuration
+          process.  See the following links for more information:
+            - https://www.cisco.com/c/en/us/products/collateral/switches/nexus-9000-series-switches/white-paper-c11-744191.html
+            - https://blogs.cisco.com/datacenter/telemetry-in-action-netconf-and-gnmi-with-a-custom-built-collector
+            - https://github.com/cisco-ie/cisco-gnmi-python
+
+        - NX-OS switch with software version 9.3(5), or later
+
+    Usage:
+        python3 gnmi.py
+"""
 
 # Imports
 from cisco_gnmi import ClientBuilder
@@ -26,11 +41,12 @@ builder.set_call_authentication(**GNMI_AUTH)
 
 client = builder.construct()
 
-# Start GNMI subscription
+# Start gNMI subscription
 gnmi_subscription = client.subscribe_xpaths(
     XPATH_CPU_0_INSTANT
 )
 
+# Display gNMI subscription data every 10 seconds
 while gnmi_subscription.is_active() is True:
     print(f'{gnmi_subscription.next()}\n')
     sleep(10)
